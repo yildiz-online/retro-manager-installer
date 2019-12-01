bf2f8d97e255d85ed524b224131f482ff6e86a7d
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-01 10:57:23 +0100

    [FIX] Paths.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cmake ../../c++ \
+cmake . \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
