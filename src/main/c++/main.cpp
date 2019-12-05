7db98e4fba56e5301a2831cbb576ca4c3a01276f
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-05 06:53:49 +0100

    Update main.cpp

--- src/main/c++/main.cpp
+++ src/main/c++/main.cpp
@@ -28,11 +28,22 @@ int main () {
         //@tar -zxvf java.tar.gz
         
         struct archive *a;
-	    a = archive_read_new();
-
-	    archive_read_support_filter_gzip(a);
-	    archive_read_support_format_raw(a);
-
+	a = archive_read_new();
+	int r;
+        archive_read_support_filter_gzip(a);
+        archive_read_support_format_raw(a);
+	r = archive_read_open_filename(a, "java.tar.gz", 10240);
+        if (r != ARCHIVE_OK) {
+		
+	}
+        while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
+            printf("%s\\n",archive_entry_pathname(entry));
+            archive_read_data_skip(a);  // Note 2
+        }
+        r = archive_read_free(a);  // Note 3
+        if (r != ARCHIVE_OK) {
+	
+	}
         
         log << "Unpack java.tar.gz complete." << std::endl;
     } else {
