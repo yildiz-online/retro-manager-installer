719c37ae1ebb6d7d7921d1b144a3ce79996e5f17
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-28 13:56:20 +0100

    Update build.sh

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,1 +1,13 @@
-return 0
+#!/usr/bin/env bash
+
+cmake . \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64"
+
+make
+r1=$?
+
+mkdir -p ../../../../target/classes/linux64/
+cp Play50hz-server.bin ../../../../target/classes/linux64/Play50hz-server.bin
+
+exit ${r1}
