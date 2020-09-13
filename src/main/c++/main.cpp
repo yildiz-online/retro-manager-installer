e17adaa78c0ef2ec6624bd7687f51c50df2b4312
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2020-09-13 08:09:01 +0200

    Update main.cpp

--- src/main/c++/main.cpp
+++ src/main/c++/main.cpp
@@ -1,9 +1,3 @@
-#ifdef __linux__ 
-    #include <arpa/inet.h>
-    #include <unistd.h>
-#elif _WIN32
-    #include <winsock2.h>
-#endif
 #include <string.h>
 #include <iostream>
 #include <fstream>
@@ -13,7 +7,24 @@
 #include <archive_entry.h>
 #include "httprequest.h"
 
+#ifdef __linux__
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    const std::string javaUrl = std::string("http://files.yildiz-games.be/java_jre_linux64.tar.gz");
+    const std::string javaVersionUrl = std::string("http://files.yildiz-games.be/release_linux64");
+    const std::string javaFile = std::string("java/bin/java");
+#elif _WIN32
+    #include <winsock2.h>
+
+    const std::string javaUrl = std::string("http://files.yildiz-games.be/java_jre_win64.tar.gz");
+    const std::string javaVersionUrl = std::string("http://files.yildiz-games.be/release");
+    const std::string javaFile = std::string("java/bin/java.exe");
+#endif
+
 std::ofstream log;
+
+void print(const std::string& message);
 
 bool isFileExists (const std::string& name);
 
@@ -21,9 +32,9 @@ void downloadFile(const std::string& fileName, const std::string& url);
 
 int compareFiles(const std::string& file1, const std::string file2);
 
-void runApp();
+void getJava();
 
-void(print(const std::string& message);
+void runApp();
 
 void warn(const char *f, const char *m);
 
@@ -38,81 +49,63 @@ static int verbose = 0;
 int main () {
     log.open("retro-manager.log", std::ios::out | std::ios::trunc );
     print("Checking java availability");
-#ifdef __linux__ 
-    if(!isFileExists("java/bin/java")) {
-        print("Play50hz java not found, starting download...");
-        downloadFile("java.tar.gz", "http://files.yildiz-games.be/java_jre_linux64.tar.gz");
-#elif _WIN32
-    if(!isFileExists("java/bin/java.exe")) {
-        print("Play50hz java not found, starting download...");
-        downloadFile("java.tar.gz", "http://files.yildiz-games.be/java_jre_win64.tar.gz");
-#endif
-        print("Java download complete.");
-        print("Unpacking java.tar.gz...");
-        extract( "java.tar.gz", 1, 0);
-        print("Unpack java.tar.gz complete.");
+    if(!isFileExists(javaFile)) {
+	print("Play50hz has its own java virtual machine, different from the one you mave have already installed manually.");
+        print("Play50hz java specific version not found, downloading it..");
+        getJava();
     } else {
-        print("Java found, checking version...");
-#ifdef __linux__ 
-    downloadFile("expected-release", "http://files.yildiz-games.be/release_linux64");  
-#elif _WIN32
-    downloadFile("expected-release", "http://files.yildiz-games.be/release");  
-#endif
+        print("Java found, checking version...");    
+        downloadFile("expected-release", javaVersionUrl);  
         if(!compareFiles("java/release", "expected-release")) {
-	    print("Java version not matching, downloading the correct one...");
-#ifdef __linux__ 
-    downloadFile("java.tar.gz", "http://files.yildiz-games.be/java_jre_linux64.tar.gz");
-#elif _WIN32
-    downloadFile("java.tar.gz", "http://files.yildiz-games.be/java_jre_win64.tar.gz");
-#endif
-            print("Java download complete.");
-            print("Unpacking java.tar.gz...");
-            extract( "java.tar.gz", 1, 0);
-            print("Unpack java.tar.gz complete.");
+	    print("Play50hz java version not matching, downloading the latest one..."); 
+            getJava();
         } else { 
-	    print("Java version is correct.");
+	    print("Java version is correct.");	
 	}
     }
     print("Downloading latest version of the application...");
-    downloadFile("play50hz-server.jar", "http://play50hz-data.yildiz-games.be/launcher.jar");  
-    print(Download latest version of the launcher complete.");
-    print("Starting Play50hz manager...");
+    downloadFile("play50hz-manager.jar", "http://files.yildiz-games.be/play50hz/launcher/manager-launcher.jar");
+    print("Download latest version of the launcher complete.");	
+    print("Starting Play50hz...");	
     runApp();
     
     return 0;
 }
 
+void getJava() {
+    downloadFile("java.tar.gz", javaUrl); 
+    print("Java download complete.");   
+    print("Unpacking java.tar.gz...");    
+    extract( "java.tar.gz", 1, 0);
+    print("Unpack java.tar.gz complete.");	
+}
+
+std::string workingdir() {
 #ifdef __linux__ 
-    std::string workingdir() {
-        return get_current_dir_name();
-    }
-
-    void runApp() {
-        std::string cmd = "\"" + workingdir() +  "/java/bin/java" + "\"" + " -jar play50hz-server.jar";
-        system(cmd.c_str());
-    } 
+    return get_current_dir_name();
 #elif _WIN32
-    std::string workingdir() {
-        char buf[MAX_PATH];
-        GetCurrentDirectoryA(MAX_PATH, buf);
-        return std::string(buf);
-    }
-
-    void runApp() {
-        std::string cmd = "\"" + workingdir() +  "/java/bin/java.exe" + "\"" + " -jar play50hz-server.jar";
-        system(cmd.c_str());
-    }
+    char buf[MAX_PATH];
+    GetCurrentDirectoryA(MAX_PATH, buf);
+    return std::string(buf);
 #endif
+}
 
+void runApp() {
+    std::string cmd = "\"" + workingdir() +  "/" + javaFile + "\"" + " -jar play50hz-manager.jar";
+    system(cmd.c_str());
+}
 
-static size_t writeData(void *ptr, size_t size, size_t nmemb, void *stream)
-{
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+static size_t writeData(void *ptr, size_t size, size_t nmemb, void *stream) {
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
+}
+	
+void print(const std::string& message) {
+    log << message << std::endl;
+    std::cout << message << std::endl;	
 }
 
 void downloadFile(const std::string& fileName, const std::string& url) {
-    print("This can take some time, please wait...");
     try {
         http::Request request(url);
         const http::Response response = request.send("GET");
@@ -120,12 +113,7 @@ void downloadFile(const std::string& fileName, const std::string& url) {
         outfile.write(reinterpret_cast<const char*>(response.body.data()), static_cast<std::streamsize>(response.body.size()));
     } catch (const std::exception& e) {
         std::cerr << "Request failed, error: " << e.what() << '\n';
-    }	
-}
-	
-void print(const std::string& message) {
-    log << message << std::endl;
-    std::cout << message << std::endl;	
+    }
 }
 
 inline bool isFileExists (const std::string& name) {
@@ -137,13 +125,11 @@ inline bool isFileExists (const std::string& name) {
     }   
 }
 
-static void
-extract(const char *filename, int do_extract, int flags)
-{
-	struct archive *a;
-	struct archive *ext;
-	struct archive_entry *entry;
-	int r;
+static void extract(const char *filename, int do_extract, int flags) {
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int r;
 
 	a = archive_read_new();
 	ext = archive_write_disk_new();
@@ -191,9 +177,7 @@ extract(const char *filename, int do_extract, int flags)
   	archive_write_free(ext);
 }
 
-static int
-copy_data(struct archive *ar, struct archive *aw)
-{
+static int copy_data(struct archive *ar, struct archive *aw) {
 	int r;
 	const void *buff;
 	size_t size;
